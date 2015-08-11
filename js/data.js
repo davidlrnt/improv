28bef82fe9f2b2a5b1f0e6baa80ad8248a56a5dd
David Lorente <david.l.castilla@gmail.com>
2015-08-11 17:45:17 -0400

    update on js

--- js/data.js
+++ js/data.js
@@ -1,3 +1,8 @@
 var playlist = [
-['https://www.youtube.com/embed/aHqnFWLP7wA?start=36&end=38&autoplay=1']
+['https://www.youtube.com/embed/aHqnFWLP7wA?start=36&end=38&autoplay=1', 'https://www.youtube.com/embed/s3Qdsn401H0?start=36&end=38&autoplay=1'],
+['https://www.youtube.com/embed/J6g53Hm0rq4?start=20&end=22&autoplay=1'],
+['https://www.youtube.com/embed/SWSpRyCDfZE?start=10&end=13&autoplay=1']
 ];
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $( document ).ready(function(){
+	var randomPlaylist = shuffle(playlist);
+	console.log(randomPlaylist[0][0])
+
+
+ $('#video').attr('src',randomPlaylist[0][0]);   
+
+// $(function(){
+//   $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
+
+//   $(window).resize(function(){
+//     $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
+//   });
 
 });
